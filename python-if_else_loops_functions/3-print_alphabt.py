e64061ae02cd84db57204b6433bba9f14a20e56b
Jojo <j.kanu@alustudent.com>
2024-06-28 05:12:10 -0700

    done

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,2 +1,3 @@
-#!/usr/bin/python3
-print("".join([chr(i) for i in range(97, 123) if chr(i) not in 'qe']), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print(chr(i), end='')
