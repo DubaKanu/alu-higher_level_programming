69be955345524692c8471a06f5197ad03ffad1e0
Jojo <j.kanu@alustudent.com>
2024-06-28 13:04:51 -0700

    done

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for c in str:
-        c = ord(c)
-        if ((c >= 97) and (c <= 122)):
-            c -= 32
-        print("{}".format(chr(c)), end='')
-    print("")
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    else:
+        return False
