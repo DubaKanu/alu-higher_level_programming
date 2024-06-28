b56094693030a1a45ad25d6ed9a6536f088b4c16
Jojo <j.kanu@alustudent.com>
2024-06-28 08:50:51 -0700

    done

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def add(a, b):
+    return a + b
+
+# Example usage:
+result = add(5, 3)
+print(result)  # Output: 8
+
+result = add(-10, 7)
+print(result)  # Output: -3
+
+result = add(0, 100)
+print(result)  # Output: 100
