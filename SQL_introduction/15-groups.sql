55f6749a0b637649d7c8b22fdde1d3723bd6b05d
Jojo <j.kanu@alustudent.com>
2024-07-16 14:56:34 -0700

    done

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,6 @@
--- Computes the average score of all records in the table second_table.
-SELECT AVG(`score`) AS `average`
-FROM `second_table`;
+-- Lists the number of records with the same score in the table second_table.
+-- Records are ordered by descending count.
+SELECT `score`, COUNT(*) AS `number`
+FROM `second_table`
+GROUP BY `score`
+ORDER BY `number` DESC;
