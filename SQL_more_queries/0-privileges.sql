dda3408e59f3ac02d5ee20d8c122f1c0b9ccab58
Jojo <j.kanu@alustudent.com>
2024-07-16 16:18:05 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -4,5 +4,5 @@ SELECT VERSION() AS 'mysql_version';
 -- Check if user_0d_1 is root (all privileges)
 SHOW GRANTS FOR 'user_0d_1'@'%'; 
 
--- Show grants for user_0d_2 (assuming no root privileges)
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- Show grants for user_0d_2 focusing on user_2_db
+SHOW GRANTS FOR 'user_0d_2'@'localhost' ON `user_2_db`*;
