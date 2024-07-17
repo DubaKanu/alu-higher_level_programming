68c470ad567db238430ea39074867bc0bad10b4d
Jojo <j.kanu@alustudent.com>
2024-07-17 11:39:31 -0700

    done

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,8 +1,4 @@
--- Connect to MySQL server
-SELECT VERSION() AS 'mysql_version'; 
-
--- Check if user_0d_1 is root (all privileges)
-SHOW GRANTS FOR 'user_0d_1'@'%'; 
-
--- Show grants for user_0d_2 focusing on user_2_db
-SHOW GRANTS FOR 'user_0d_2'@'localhost' ON `user_2_db`*;
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
